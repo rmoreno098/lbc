812e2c85a6d596e18fdd3ca5c37627f2a0152b2d
roman <roman.moreno226@live.com>
2025-03-08 19:31:55 -0600

    created structs LB and Server, added functions for re-use

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-# all: main
+all: main
 
 main: main.o
 	gcc main.o -o main
--- main.c
+++ main.c
@@ -11,18 +11,59 @@
 #define BACKLOG 5
 #define DEFAULT_BUFFER_SIZE 4096
 
-struct addrinfo hints, *res;
+typedef struct Server {
+  char *port;
+  char *domain;
+  int sock_fd;
+} Server;
 
-void handle_get(int client_fd, char *path) {}
+typedef struct LoadBalancer {
+  Server *server;
+  struct addrinfo *res;
+} LoadBalancer;
 
-void handle_post(int client_fd, char *path) {}
+Server servers[] = {{"127.0.0.1", "12345"}, {"127.0.0.1", "12346"}};
 
-void parse_request(int client_fd) {
+LoadBalancer *load_balancer_init();
+void server_connect(char *domain, char *port);
+void server_disconnect(Server *server);
+void handle_client(int client_fd);
+
+int main() {
+  LoadBalancer *lb = load_balancer_init();
+
+  int client_fd;
+  struct sockaddr_storage client_addr;
+  socklen_t client_addr_len = sizeof(client_addr);
+
+  while (1) {
+    client_fd = accept(lb->server->sock_fd, (struct sockaddr *)&client_addr,
+                       &client_addr_len);
+    handle_client(client_fd);
+  }
+
+  server_disconnect(lb->server);
+  freeaddrinfo(lb->res);
+
+  return 0;
+}
+
+void server_connect(char *domain, char *port) {}
+
+void server_disconnect(Server *server) {
+  close(server->sock_fd);
+  printf("Server shutdown\n");
+  free(server);
+}
+
+void handle_client(int client_fd) {
   char request[DEFAULT_BUFFER_SIZE]; // Make dynamic
 
+  // Handle this better with a loop
   int bytes_recv = recv(client_fd, request, DEFAULT_BUFFER_SIZE, 0);
-  if (bytes_recv == DEFAULT_BUFFER_SIZE) {
-    perror("Buffer full, increase buffer size\n");
+  if (bytes_recv < 0) {
+    perror("recv error");
+    close(client_fd);
     exit(1);
   }
 
@@ -31,7 +72,7 @@ void parse_request(int client_fd) {
   char *path = strtok(NULL, " ");
   char *protocol = strtok(NULL, "\r\n");
 
-  if (strstr(protocol, "HTTP/1.1") <= 0) {
+  if (strcmp(protocol, "HTTP/1.1") != 0) {
     char *response = "HTTP/1.1 400 Bad Request\nContent-Type: text/html\n\n";
     send(client_fd, response, strlen(response), 0);
     close(client_fd);
@@ -39,9 +80,9 @@ void parse_request(int client_fd) {
   }
 
   if (strcmp(method, "GET") == 0) {
-    handle_get(client_fd, path);
+    // handle_get(client_fd, path);
   } else if (strcmp(method, "POST") == 0) {
-    handle_post(client_fd, path);
+    // handle_post(client_fd, path);
   } else {
     char *response = "HTTP/1.1 405 Method Not Allowed\nContent-Type: "
                      "text/html\n\n<html><body><h1>405 Method Not "
@@ -50,26 +91,42 @@ void parse_request(int client_fd) {
     close(client_fd);
     exit(1);
   }
+
+  char *response =
+      "HTTP/1.1 200 OK\nContent-Type: text/html\n\n<html><body><h1>Hello, "
+      "World!</h1></body></html>";
+  int x = send(client_fd, response, strlen(response), 0);
+  if (x != strlen(response)) {
+    printf("Data send did not equal | actual: %d | expected: %lu\n", x,
+           strlen(response));
+  }
+  close(client_fd);
 }
 
-int main() {
+LoadBalancer *load_balancer_init() {
+  struct addrinfo hints;
   memset(&hints, 0, sizeof(hints));
+
+  LoadBalancer *lb = malloc(sizeof(struct LoadBalancer));
+  lb->server = malloc(sizeof(struct Server));
+
   hints.ai_family = AF_UNSPEC;
   hints.ai_socktype = SOCK_STREAM;
 
-  int status = getaddrinfo(DOMAIN, PORT, &hints, &res);
+  int status = getaddrinfo(DOMAIN, PORT, &hints, &lb->res);
   if (status != 0) {
     fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
     exit(1);
   }
 
-  int sock_fd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+  int sock_fd =
+      socket(lb->res->ai_family, lb->res->ai_socktype, lb->res->ai_protocol);
   if (sock_fd == -1) {
     perror("socket error");
     exit(1);
   }
 
-  if (bind(sock_fd, res->ai_addr, res->ai_addrlen) < 0) {
+  if (bind(sock_fd, lb->res->ai_addr, lb->res->ai_addrlen) < 0) {
     perror("bind error");
     close(sock_fd);
     exit(1);
@@ -82,29 +139,7 @@ int main() {
   }
 
   printf("Server listening on %s:%s...\n", DOMAIN, PORT);
+  lb->server->sock_fd = sock_fd;
 
-  int client_fd;
-  struct sockaddr_storage client_addr;
-  socklen_t client_addr_len = sizeof(client_addr);
-
-  while (1) {
-    client_fd =
-        accept(sock_fd, (struct sockaddr *)&client_addr, &client_addr_len);
-
-    parse_request(client_fd);
-
-    char *response =
-        "HTTP/1.1 200 OK\nContent-Type: text/html\n\n<html><body><h1>Hello, "
-        "World!</h1></body></html>";
-    int x = send(client_fd, response, strlen(response), 0);
-    if (x != strlen(response)) {
-      printf("Data send did not equal | actual: %d | expected: %lu\n", x,
-             strlen(response));
-    }
-
-    close(client_fd);
-  }
-  freeaddrinfo(res);
-
-  return 0;
+  return lb;
 }
