fdeb3400d3033c1fff0217c99f30783d15f2a840
roman <roman.moreno226@live.com>
2025-04-16 19:20:30 -0500

    added graceful shutdown to load balancer

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <arpa/inet.h>
 #include <asm-generic/socket.h>
+#include <errno.h>
 #include <netdb.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -13,6 +15,8 @@
 #define PORT "48963"
 #define BACKLOG 5
 #define DEFAULT_BUFFER_SIZE 4096
+
+volatile sig_atomic_t stop_server = 0;
 
 typedef struct Server {
   char *domain;
@@ -28,10 +32,16 @@ typedef struct LoadBalancer {
 Server servers[] = {{"127.0.0.1", "12345"}, {"127.0.0.1", "12346"}};
 
 LoadBalancer *load_balancer_init();
-char* handle_client(int client_fd, struct sockaddr_storage client_addr);
+char *handle_client(int client_fd, struct sockaddr_storage client_addr);
 void server_disconnect(Server *server);
 int server_connect(char *domain, char *port);
 void epoll_ctl_add(int epfd, int fd);
+
+static void signal_handler(int signum) {
+  if (signum == SIGINT) {
+    stop_server = 1;
+  }
+}
 
 int main() {
   LoadBalancer *lb = load_balancer_init();
@@ -40,22 +50,37 @@ int main() {
   struct sockaddr_storage client_addr;
   socklen_t client_addr_len = sizeof(client_addr);
 
-  while (1) {
+  struct sigaction sa;
+  sa.sa_handler = signal_handler;
+  sigemptyset(&sa.sa_mask);
+  sa.sa_flags = 0;
+  if (sigaction(SIGINT, &sa, NULL) == -1) {
+    perror("SIGINT");
+    return -1;
+  }
+
+  while (!stop_server) {
     client_fd = accept(lb->server->sock_fd, (struct sockaddr *)&client_addr,
                        &client_addr_len);
-
-    if (client_fd >= 0) {
-      int epoll_fd = epoll_create(1);
-      if (epoll_fd <= 0) {
-        perror("epoll_create");
-        exit(1);
-      }
-      epoll_ctl_add(epoll_fd, lb->server->sock_fd);
+    if (client_fd < 0) {
+      if (errno == EINTR && stop_server)
+        break;
+      perror("accept");
+      continue;
     }
 
-    char* client_request = handle_client(client_fd, client_addr);
+    int epoll_fd = epoll_create(1);
+    if (epoll_fd <= 0) {
+      perror("epoll_create");
+      exit(1);
+    }
+    epoll_ctl_add(epoll_fd, lb->server->sock_fd);
+
+    char *client_request = handle_client(client_fd, client_addr);
     int backend_socket = server_connect(servers[0].domain, servers[0].port);
-    int client_send = send(backend_socket, client_request, strlen(client_request), 0); // Track number of bytes sent
+    int client_send =
+        send(backend_socket, client_request, strlen(client_request),
+             0); // Track number of bytes sent
     if (client_send < 0) {
       perror("send error");
       close(backend_socket);
@@ -73,18 +98,22 @@ int main() {
 
     int client_response = send(client_fd, request, strlen(request), 0);
     close(client_fd);
-    printf("Received %d bytes from backend server and sent %d bytes to client\n", bytes_recv, client_response);
+    printf(
+        "Received %d bytes from backend server and sent %d bytes to client\n",
+        bytes_recv, client_response);
   }
 
+  printf("\nShutting down...\n");
   server_disconnect(lb->server);
   freeaddrinfo(lb->res);
+  printf("Load balancer shutdown.\n");
 
   return 0;
 }
 
 void epoll_ctl_add(int epfd, int fd) {
   struct epoll_event event;
-  event.events = EPOLLIN | EPOLLOUT;
+  event.events = EPOLLIN | EPOLLET;
   event.data.fd = fd;
 
   if (epoll_ctl(epfd, EPOLL_CTL_ADD, fd, &event) < 0) {
@@ -121,12 +150,11 @@ int server_connect(char *domain, char *port) {
 
 void server_disconnect(Server *server) {
   close(server->sock_fd);
-  printf("Server shutdown\n");
   free(server);
 }
 
-char* handle_client(int client_fd, struct sockaddr_storage client_addr) {
-  char* request = malloc(DEFAULT_BUFFER_SIZE);
+char *handle_client(int client_fd, struct sockaddr_storage client_addr) {
+  char *request = malloc(DEFAULT_BUFFER_SIZE);
   bzero(request, DEFAULT_BUFFER_SIZE);
 
   // Handle this better with a loop
